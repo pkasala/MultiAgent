f611ce9103930fe8d720da279e2e8e8435acc362
pkasala <p.kasala@gmail.com>
2019-08-15 01:36:49 +0200

    Add files via upload

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,62 +13,75 @@ def hidden_init(layer):
 
 
 class Actor(nn.Module):
+    """Actor (Policy) Model."""
 
     def __init__(self, state_size,action_size,seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            hidden_units (array): Number of nodes for layers
+            seed (int): Random seed
+            gate (function): activation function
+            final_gate (function): final activation function
+        """
         super(Actor, self).__init__()
-        #use the global seed
         self.seed = torch.manual_seed(seed)
-        #include the batch normalization for becasue we normalize the batch input data, and therfore reduce internal covariant
         self.normalizer = nn.BatchNorm1d(state_size)
-        #basic full connected input layer
-        self.input = nn.Linear(state_size, 512)
-        self.hidden1 = nn.Linear(512,256)
-        self.output = nn.Linear(256, action_size)
-        #this ensure to init the weight closer to 0
+        self.input = nn.Linear(state_size, 500)
+        self.hidden1 = nn.Linear(500,300)
+        self.output = nn.Linear(300, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.input.weight.data.uniform_(*hidden_init(self.input))
         self.hidden1.weight.data.uniform_(*hidden_init(self.hidden1))
-        #fixed values of output layer - this is too small for standard init
         self.output.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, states):
-        #normalize the batch input
+        """Build an actor (policy) network that maps states -> actions."""
         x = self.normalizer(states)
         x = F.relu(self.input(x))
         x = F.relu(self.hidden1(x))
-        #output action is in -1 +1 - use tanh
         x = torch.tanh(self.output(x))
         return x
 
 class Critic(nn.Module):
+    """Critic (Value) Model."""
 
     def __init__(self, state_size,action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            hidden_units (array): Number of nodes for layers
+            seed (int): Random seed
+            gate (function): activation function
+        """
         super(Critic, self).__init__()
-        #use global seed
         self.seed = torch.manual_seed(seed)
         self.dropout = nn.Dropout(p=0.2)
-        # include the batch normalization for becasue we normalize the batch input data, and therfore reduce internal covariant
         self.normalizer = nn.BatchNorm1d(state_size)
-        self.input = nn.Linear(state_size, 512)
-        #adjust the network with action state
-        self.hidden1_action = nn.Linear(512+action_size, 256)
-        self.output = nn.Linear(256,1)
-        # this ensure to init the weight closer to 0
+        self.input = nn.Linear(state_size, 500)
+        self.hidden1_action = nn.Linear(500+action_size, 300)
+        self.hidden2 = nn.Linear(300, 200)
+        self.output = nn.Linear(200,1)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.input.weight.data.uniform_(*hidden_init(self.input))
         self.hidden1_action.weight.data.uniform_(*hidden_init(self.hidden1_action))
+        self.hidden2.weight.data.uniform_(*hidden_init(self.hidden2))
         self.output.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, states, actions):
-        #normalize the input batch
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         x = self.normalizer(states)
-        #feed forward layers
         x = F.relu(self.input(x))
         y = torch.cat((x, actions), dim=1)
         y = F.relu(self.hidden1_action(y))
+        y = F.relu(self.hidden2(y))
         y = self.dropout(y)
         return self.output(y)
